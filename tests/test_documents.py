0f15507ca7abe7ef511edd5eabcaf842a7a484ab
Karl Hobley <karlhobley10@gmail.com>
2015-01-26 15:27:29 +0000

    Tests for listing views

---
+++ tests/test_documents.py
@@ -0,0 +1,171 @@
+import json
+import unittest
+
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from wagtail.wagtaildocs.models import Document
+
+from . import models
+
+
+class TestDocumentListing(TestCase):
+    fixtures = ['wagtailapi_tests.json']
+
+    def get_response(self, **params):
+        return self.client.get(reverse('wagtailapi_v1_document_listing'), params)
+
+    def get_document_id_list(self, content):
+        return [page['id'] for page in content['documents']]
+
+
+    # BASIC TESTS
+
+    def test_status_code(self):
+        response = self.get_response()
+        self.assertEqual(response.status_code, 200)
+
+    def test_content_type_header(self):
+        response = self.get_response()
+        self.assertEqual(response['Content-type'], 'application/json')
+
+    def test_valid_json(self):
+        response = self.get_response()
+
+        # Will crash if there's a problem
+        json.loads(response.content.decode('UTF-8'))
+
+    def test_meta_section_is_present(self):
+        response = self.get_response()
+        content = json.loads(response.content.decode('UTF-8'))
+        self.assertIn('meta', content)
+        self.assertIsInstance(content['meta'], dict)
+
+    def test_total_count_is_present(self):
+        response = self.get_response()
+        content = json.loads(response.content.decode('UTF-8'))
+        self.assertIn('total_count', content['meta'])
+        self.assertIsInstance(content['meta']['total_count'], int)
+
+    def test_documents_section_is_present(self):
+        response = self.get_response()
+        content = json.loads(response.content.decode('UTF-8'))
+        self.assertIn('documents', content)
+        self.assertIsInstance(content['documents'], list)
+
+    def test_total_count(self):
+        response = self.get_response()
+        content = json.loads(response.content.decode('UTF-8'))
+        self.assertEqual(content['meta']['total_count'], Document.objects.count())
+
+
+    # FILTERING
+
+    def test_filtering_exact_filter(self):
+        response = self.get_response(title='James Joyce')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        document_id_list = self.get_document_id_list(content)
+        self.assertEqual(document_id_list, [2])
+
+    @unittest.expectedFailure
+    def test_filtering_unknown_field_gives_error(self):
+        response = self.get_response(not_a_field='abc')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        self.assertEqual(response.status_code, 400)
+
+
+    # ORDERING
+
+    def test_ordering_default(self):
+        response = self.get_response()
+        content = json.loads(response.content.decode('UTF-8'))
+
+        document_id_list = self.get_document_id_list(content)
+        self.assertEqual(document_id_list, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
+    def test_ordering_by_title(self):
+        response = self.get_response(order='title')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        document_id_list = self.get_document_id_list(content)
+        self.assertEqual(document_id_list, [3, 12, 10, 2, 7, 9, 8, 4, 1, 5, 11, 6])
+
+    @unittest.expectedFailure
+    def test_ordering_by_title_backwards(self):
+        response = self.get_response(order='-title')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        document_id_list = self.get_document_id_list(content)
+        self.assertEqual(document_id_list, [6, 11, 5, 1, 4, 8, 9, 9, 2, 10, 12, 3])
+
+    @unittest.expectedFailure
+    def test_ordering_by_unknown_field_gives_error(self):
+        response = self.get_response(order='not_a_field')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        self.assertEqual(response.status_code, 400)
+
+
+    # LIMIT
+
+    def test_limit_only_two_results_returned(self):
+        response = self.get_response(limit=2)
+        content = json.loads(response.content.decode('UTF-8'))
+
+        self.assertEqual(len(content['documents']), 2)
+
+    def test_limit_total_count(self):
+        response = self.get_response(limit=2)
+        content = json.loads(response.content.decode('UTF-8'))
+
+        # The total count must not be affected by "limit"
+        self.assertEqual(content['meta']['total_count'], Document.objects.count())
+
+    @unittest.expectedFailure
+    def test_limit_not_integer_gives_error(self):
+        response = self.get_response(limit='abc')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        self.assertEqual(response.status_code, 400)
+
+
+    # OFFSET
+
+    def test_offset_5_usually_appears_5th_in_list(self):
+        response = self.get_response()
+        content = json.loads(response.content.decode('UTF-8'))
+        document_id_list = self.get_document_id_list(content)
+        self.assertEqual(document_id_list.index(5), 4)
+
+    def test_offset_5_moves_after_offset(self):
+        response = self.get_response(offset=4)
+        content = json.loads(response.content.decode('UTF-8'))
+        document_id_list = self.get_document_id_list(content)
+        self.assertEqual(document_id_list.index(5), 0)
+
+    def test_offset_total_count(self):
+        response = self.get_response(offset=10)
+        content = json.loads(response.content.decode('UTF-8'))
+
+        # The total count must not be affected by "offset"
+        self.assertEqual(content['meta']['total_count'], Document.objects.count())
+
+    @unittest.expectedFailure
+    def test_offset_not_integer_gives_error(self):
+        response = self.get_response(offset='abc')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        self.assertEqual(response.status_code, 400)
+
+
+    # SEARCH
+
+    def test_search_for_james_joyce(self):
+        response = self.get_response(search='james')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        document_id_list = self.get_document_id_list(content)
+
+        self.assertEqual(set(document_id_list), set([2]))
---
+++ tests/test_pages.py
@@ -0,0 +1,330 @@
+import json
+import unittest
+
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from wagtail.wagtailcore.models import Page
+
+from . import models
+
+
+def get_total_page_count():
+    # Need to take away 1 as the root page is invisible over the API
+    return Page.objects.live().public().count() - 1
+
+
+class TestPageListing(TestCase):
+    fixtures = ['wagtailapi_tests.json']
+
+    def get_response(self, **params):
+        return self.client.get(reverse('wagtailapi_v1_page_listing'), params)
+
+    def get_page_id_list(self, content):
+        return [page['id'] for page in content['pages']]
+
+
+    # BASIC TESTS
+
+    def test_status_code(self):
+        response = self.get_response()
+        self.assertEqual(response.status_code, 200)
+
+    def test_content_type_header(self):
+        response = self.get_response()
+        self.assertEqual(response['Content-type'], 'application/json')
+
+    def test_valid_json(self):
+        response = self.get_response()
+
+        # Will crash if there's a problem
+        json.loads(response.content.decode('UTF-8'))
+
+    def test_meta_section_is_present(self):
+        response = self.get_response()
+        content = json.loads(response.content.decode('UTF-8'))
+        self.assertIn('meta', content)
+        self.assertIsInstance(content['meta'], dict)
+
+    def test_total_count_is_present(self):
+        response = self.get_response()
+        content = json.loads(response.content.decode('UTF-8'))
+        self.assertIn('total_count', content['meta'])
+        self.assertIsInstance(content['meta']['total_count'], int)
+
+    def test_pages_section_is_present(self):
+        response = self.get_response()
+        content = json.loads(response.content.decode('UTF-8'))
+        self.assertIn('pages', content)
+        self.assertIsInstance(content['pages'], list)
+
+    def test_total_count(self):
+        response = self.get_response()
+        content = json.loads(response.content.decode('UTF-8'))
+        self.assertEqual(content['meta']['total_count'], get_total_page_count())
+
+    def test_unpublished_pages_dont_appear_in_list(self):
+        total_count = get_total_page_count()
+
+        page = models.BlogEntryPage.objects.get(id=16)
+        page.unpublish()
+
+        response = self.get_response()
+        content = json.loads(response.content.decode('UTF-8'))
+        self.assertEqual(content['meta']['total_count'], total_count - 1)
+
+    def test_private_pages_dont_appear_in_list(self):
+        total_count = get_total_page_count()
+
+        page = models.BlogIndexPage.objects.get(id=5)
+        page.view_restrictions.create(password='test')
+
+        new_total_count = get_total_page_count()
+        self.assertNotEqual(total_count, new_total_count)
+
+        response = self.get_response()
+        content = json.loads(response.content.decode('UTF-8'))
+        self.assertEqual(content['meta']['total_count'], new_total_count)
+
+
+    # TYPE FILTER
+
+    def test_type_filter_results_are_all_blog_entries(self):
+        response = self.get_response(type='tests.BlogEntryPage')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        for page in content['pages']:
+            self.assertEqual(page['meta']['type'], 'tests.BlogEntryPage')
+
+    def test_type_filter_total_count(self):
+        response = self.get_response(type='tests.BlogEntryPage')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        # Total count must be reduced as this filters the results
+        self.assertEqual(content['meta']['total_count'], 3)
+
+    def test_non_existant_type_gives_error(self):
+        response = self.get_response(type='tests.IDontExist')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(content, {'message': "Type doesn't exist"})
+
+
+    # EXTRA FIELDS
+
+    def test_extra_fields_default(self):
+        response = self.get_response(type='tests.BlogEntryPage')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        for page in content['pages']:
+            self.assertEqual(page.keys(), set(['id', 'meta', 'title']))
+
+    def test_extra_fields(self):
+        response = self.get_response(type='tests.BlogEntryPage', fields='title,date,feed_image')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        for page in content['pages']:
+            self.assertEqual(page.keys(), set(['id', 'meta', 'title', 'date', 'feed_image']))
+
+    def test_extra_fields_child_relation(self):
+        response = self.get_response(type='tests.BlogEntryPage', fields='title,related_links')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        for page in content['pages']:
+            self.assertEqual(page.keys(), set(['id', 'meta', 'title', 'related_links']))
+            self.assertIsInstance(page['related_links'], list)
+
+    def test_extra_fields_doesnt_work_without_type(self):
+        response = self.get_response(fields='title,related_links')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        for page in content['pages']:
+            self.assertEqual(page.keys(), set(['id', 'meta', 'title']))
+
+    @unittest.expectedFailure
+    def test_extra_fields_unknown_field_gives_error(self):
+        response = self.get_response(fields='abc')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        self.assertEqual(response.status_code, 400)
+
+
+    # FILTERING
+
+    def test_filtering_exact_filter(self):
+        response = self.get_response(title='Home page')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        page_id_list = self.get_page_id_list(content)
+        self.assertEqual(page_id_list, [2])
+
+    def test_filtering_exact_filter_on_specific_field(self):
+        response = self.get_response(type='tests.BlogEntryPage', date='2013-12-02')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        page_id_list = self.get_page_id_list(content)
+        self.assertEqual(page_id_list, [16])
+
+    def test_filtering_doesnt_work_on_specific_fields_without_type(self):
+        response = self.get_response(date='2013-12-02')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        # Everything should be returned
+        self.assertEqual(content['meta']['total_count'], get_total_page_count())
+
+    @unittest.expectedFailure
+    def test_filtering_unknown_field_gives_error(self):
+        response = self.get_response(not_a_field='abc')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        self.assertEqual(response.status_code, 400)
+
+
+    # CHILD OF FILTER
+
+    def test_child_of_filter(self):
+        response = self.get_response(child_of=5)
+        content = json.loads(response.content.decode('UTF-8'))
+
+        page_id_list = self.get_page_id_list(content)
+        self.assertEqual(page_id_list, [16, 18, 19])
+
+    def test_child_of_with_type(self):
+        response = self.get_response(type='tests.EventPage', child_of=5)
+        content = json.loads(response.content.decode('UTF-8'))
+
+        page_id_list = self.get_page_id_list(content)
+        self.assertEqual(page_id_list, [])
+
+    def test_child_of_unknown_page_gives_error(self):
+        response = self.get_response(child_of=1000)
+        content = json.loads(response.content.decode('UTF-8'))
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(content, {'message': "Parent page doesn't exist"})
+
+
+    # ORDERING
+
+    def test_ordering_default(self):
+        response = self.get_response()
+        content = json.loads(response.content.decode('UTF-8'))
+
+        page_id_list = self.get_page_id_list(content)
+        self.assertEqual(page_id_list, [2, 4, 8, 9, 5, 16, 18, 19, 6, 10, 15, 17, 21, 22, 23, 20, 13, 14, 12])
+
+    def test_ordering_by_title(self):
+        response = self.get_response(order='title')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        page_id_list = self.get_page_id_list(content)
+        self.assertEqual(page_id_list, [21, 22, 19, 23, 5, 16, 18, 12, 14, 8, 9, 4, 2, 13, 20, 17, 6, 10, 15])
+
+    @unittest.expectedFailure
+    def test_ordering_by_title_backwards(self):
+        response = self.get_response(order='-title')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        page_id_list = self.get_page_id_list(content)
+        self.assertEqual(page_id_list, [15, 10, 6, 17, 20, 13, 2, 4, 9, 8, 14, 12, 18, 16, 5, 23, 19, 22, 21])
+
+    def test_ordering_default_with_type(self):
+        response = self.get_response(type='tests.BlogEntryPage')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        page_id_list = self.get_page_id_list(content)
+        self.assertEqual(page_id_list, [16, 18, 19])
+
+    def test_ordering_by_title_with_type(self):
+        response = self.get_response(type='tests.BlogEntryPage', order='title')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        page_id_list = self.get_page_id_list(content)
+        self.assertEqual(page_id_list, [19, 16, 18])
+
+    def test_ordering_by_specific_field_with_type(self):
+        response = self.get_response(type='tests.BlogEntryPage', order='date')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        page_id_list = self.get_page_id_list(content)
+        self.assertEqual(page_id_list, [16, 18, 19])
+
+    @unittest.expectedFailure
+    def test_ordering_by_unknown_field_gives_error(self):
+        response = self.get_response(order='not_a_field')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        self.assertEqual(response.status_code, 400)
+
+
+    # LIMIT
+
+    def test_limit_only_two_results_returned(self):
+        response = self.get_response(limit=2)
+        content = json.loads(response.content.decode('UTF-8'))
+
+        self.assertEqual(len(content['pages']), 2)
+
+    def test_limit_total_count(self):
+        response = self.get_response(limit=2)
+        content = json.loads(response.content.decode('UTF-8'))
+
+        # The total count must not be affected by "limit"
+        self.assertEqual(content['meta']['total_count'], get_total_page_count())
+
+    @unittest.expectedFailure
+    def test_limit_not_integer_gives_error(self):
+        response = self.get_response(limit='abc')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        self.assertEqual(response.status_code, 400)
+
+
+    # OFFSET
+
+    def test_offset_5_usually_appears_5th_in_list(self):
+        response = self.get_response()
+        content = json.loads(response.content.decode('UTF-8'))
+        page_id_list = self.get_page_id_list(content)
+        self.assertEqual(page_id_list.index(5), 4)
+
+    def test_offset_5_moves_after_offset(self):
+        response = self.get_response(offset=4)
+        content = json.loads(response.content.decode('UTF-8'))
+        page_id_list = self.get_page_id_list(content)
+        self.assertEqual(page_id_list.index(5), 0)
+
+    def test_offset_total_count(self):
+        response = self.get_response(offset=10)
+        content = json.loads(response.content.decode('UTF-8'))
+
+        # The total count must not be affected by "offset"
+        self.assertEqual(content['meta']['total_count'], get_total_page_count())
+
+    @unittest.expectedFailure
+    def test_offset_not_integer_gives_error(self):
+        response = self.get_response(offset='abc')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        self.assertEqual(response.status_code, 400)
+
+
+    # SEARCH
+
+    def test_search_for_blog(self):
+        response = self.get_response(search='blog')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        page_id_list = self.get_page_id_list(content)
+
+        # Check that the results are the blog index and three blog pages
+        self.assertEqual(set(page_id_list), set([5, 16, 18, 19]))
+
+    def test_search_with_type(self):
+        response = self.get_response(type='tests.BlogEntrypage', search='blog')
+        content = json.loads(response.content.decode('UTF-8'))
+
+        page_id_list = self.get_page_id_list(content)
+
+        self.assertEqual(set(page_id_list), set([16, 18, 19]))
